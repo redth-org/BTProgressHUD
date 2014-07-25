f4a9b5caf007df31f314dbdd78202b6f39387010
ggirard-distech <ggirard@distech-controls.com>
2014-07-25 13:10:31 -0400

    Fixed toast displayed behind the keyboard if already popped.

---
+++ BTProgressHUD/NSObjectExtensions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace BigTed
+{
+    /// <summary>
+    /// Contains several extensions for NSObject and derived classes.
+    /// </summary>
+    public static class NSObjectExtensions
+    {
+
+        [DllImport("/usr/lib/libobjc.dylib")]
+        private static extern IntPtr object_getClassName(IntPtr obj);
+
+        /// <summary>
+        /// Returns the Objective-C class name of the NSObject instance.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetClassName(this NSObject obj)
+        {
+            return Marshal.PtrToStringAuto(object_getClassName(obj.Handle));
+        }
+
+    }
+}
