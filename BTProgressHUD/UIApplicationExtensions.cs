f805a67d60152240d6d0a185dcf6d1b88de1ec16
robert.sayers <robert.sayers@totalmobile.co.uk>
2021-02-02 09:59:07 +0000

    Fix null window when using alerts

--- BTProgressHUD/UIApplicationExtensions.cs
+++ BTProgressHUD/UIApplicationExtensions.cs
@@ -4,7 +4,7 @@ using UIKit;
 namespace BigTed {
     public static class UIApplicationExtensions{
         public static UIWindow GetKeyWindow(this UIApplication application) {
-            return application.Windows.FirstOrDefault(window => window.IsKeyWindow);
+            return application.Windows.FirstOrDefault(window => window.IsKeyWindow) ?? application.Windows[0];
         }
     }
 }
