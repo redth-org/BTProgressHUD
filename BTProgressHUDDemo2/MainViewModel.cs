3f5646ac30d03782c8b0309b569cd4a9cc8f33c1
Allan Ritchie <allan.ritchie@gmail.com>
2022-06-23 14:25:29 -0400

    Update MainViewModel.cs

--- BTProgressHUDDemo2/MainViewModel.cs
+++ BTProgressHUDDemo2/MainViewModel.cs
@@ -17,126 +17,93 @@ namespace BTProgressHUDDemo2
                 Create("Show", () => BTProgressHUD.Show(), true),
                 Create("Cancel problem 3", () => BTProgressHUD.Show("Cancel", () => KillAfter(), "Cancel and text"), false),
                 Create("Cancel problem 2", () => BTProgressHUD.Show("Cancel", () => KillAfter()), false),
+                Create("Cancel problem", () => BTProgressHUD.Show("Cancel", () => KillAfter(), "This is a multilinetext\nSome more text\n more text\n and again more text"), false),
+                Create("Show Message", () => BTProgressHUD.Show("Processing your image", -1, MaskType.Black), false),
+                Create("Show Success", () =>
+                {
+                    BTProgressHUD.ShowSuccessWithStatus("Great success!");
+                }, false),
+
+                Create("Show Fail", () =>
+                {
+                    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad");
+                }, false),
+
+                Create("Show Info", () =>
+                {
+                    BTProgressHUD.ShowInfoWithStatus("Notice me!");
+                }, false),
+
+                Create("Show Success (Outlined)", () =>
+                {
+                    BTProgressHUD.ShowSuccessWithStatus("Great success!", default, 1000, ImageStyle.Outline);
+                }, false),
+
+                Create("Show Fail (Outlined)", () =>
+                {
+                    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad", default, 1000, ImageStyle.Outline);
+                }, false),
+
+                Create("Show Info (Outlined)", () =>
+                {
+                    BTProgressHUD.ShowInfoWithStatus("Notice me!", default, 1000, ImageStyle.Outline);
+                }, false),
+
+                Create("Show Success (Full Outlined)", () =>
+                {
+                    BTProgressHUD.ShowSuccessWithStatus("Great success!", default, 1000, ImageStyle.OutlineFull);
+                }, false),
+
+                Create("Show Fail (Full Outlined)", () =>
+                {
+                    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad", default, 1000, ImageStyle.OutlineFull);
+                }, false),
+
+                Create("Show Info (Full Outlined)", () =>
+                {
+                    BTProgressHUD.ShowInfoWithStatus("Notice me!", default, 1000, ImageStyle.OutlineFull);
+                }, false),
+
+                Create("Toast", () =>
+                {
+                    BTProgressHUD.ShowToast("Hello from the toast", false, 1000);
+                }, false),
+
+                Create("Dismiss", () =>
+                {
+                    BTProgressHUD.Dismiss();
+                }, false),
+
+                Create("Progress", () =>
+                {
+                    progress = 0;
+                    BTProgressHUD.Show("Hello!", progress);
+                    if (timer != null)
+                    {
+                        timer.Invalidate();
+                    }
+                    timer = NSTimer.CreateRepeatingTimer(0.5f, delegate
+                    {
+                        progress += 0.1f;
+                        if (progress > 1)
+                        {
+                            timer.Invalidate();
+                            timer = null;
+                            BTProgressHUD.Dismiss();
+                        }
+                        else
+                        {
+                            BTProgressHUD.Show("Hello!", progress);
+                        }
+                    });
+                    NSRunLoop.Current.AddTimer(timer, NSRunLoopMode.Common);
+                }, false),
+
+                Create("Dismiss", () =>
+                {
+                    BTProgressHUD.Dismiss();
+                }, false)
             };
-
-            //MakeButton("Cancel problem", () =>
-            //    BTProgressHUD.Show("Cancel", () => KillAfter(), "This is a multilinetext\nSome more text\n more text\n and again more text"),
-            //    stack
-            //);
-
-            //MakeButton("Show Message", () =>
-            //{
-            //    BTProgressHUD.Show("Processing your image", -1, MaskType.Black);
-            //    KillAfter();
-            //}, stack);
-
-            //MakeButton("Show Success", () =>
-            //{
-            //    BTProgressHUD.ShowSuccessWithStatus("Great success!");
-            //}, stack);
-
-            //MakeButton("Show Fail", () =>
-            //{
-            //    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad");
-            //}, stack);
-
-            //MakeButton("Show Info", () =>
-            //{
-            //    BTProgressHUD.ShowInfoWithStatus("Notice me!");
-            //}, stack);
-
-            //MakeButton("Show Success (Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowSuccessWithStatus("Great success!", default, 1000, ImageStyle.Outline);
-            //}, stack);
-
-            //MakeButton("Show Fail (Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad", default, 1000, ImageStyle.Outline);
-            //}, stack);
-
-            //MakeButton("Show Info (Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowInfoWithStatus("Notice me!", default, 1000, ImageStyle.Outline);
-            //}, stack);
-
-            //MakeButton("Show Success (Full Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowSuccessWithStatus("Great success!", default, 1000, ImageStyle.OutlineFull);
-            //}, stack);
-
-            //MakeButton("Show Fail (Full Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowErrorWithStatus("Oh, thats bad", default, 1000, ImageStyle.OutlineFull);
-            //}, stack);
-
-            //MakeButton("Show Info (Full Outlined)", () =>
-            //{
-            //    BTProgressHUD.ShowInfoWithStatus("Notice me!", default, 1000, ImageStyle.OutlineFull);
-            //}, stack);
-
-            //MakeButton("Toast", () =>
-            //{
-            //    BTProgressHUD.ShowToast("Hello from the toast", false, 1000);
-            //}, stack);
-
-            //MakeButton("Dismiss", () =>
-            //{
-            //    BTProgressHUD.Dismiss();
-            //}, stack);
-
-            //MakeButton("Progress", () =>
-            //{
-            //    progress = 0;
-            //    BTProgressHUD.Show("Hello!", progress);
-            //    if (timer != null)
-            //    {
-            //        timer.Invalidate();
-            //    }
-            //    timer = NSTimer.CreateRepeatingTimer(0.5f, delegate
-            //    {
-            //        progress += 0.1f;
-            //        if (progress > 1)
-            //        {
-            //            timer.Invalidate();
-            //            timer = null;
-            //            BTProgressHUD.Dismiss();
-            //        }
-            //        else
-            //        {
-            //            BTProgressHUD.Show("Hello!", progress);
-            //        }
-            //    });
-            //    NSRunLoop.Current.AddTimer(timer, NSRunLoopMode.Common);
-            //}, stack);
-
-            //MakeButton("Dismiss", () =>
-            //{
-            //    BTProgressHUD.Dismiss();
-            //}, stack);
-
-            //// This demo only works on iOS 13 and above because of UIImage.GetSystemImage
-            //// But it should work with versions below if you just use a UIImage
-            //if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
-            //{
-            //    MakeButton("Success With Status", () =>
-            //        BTProgressHUD.ShowImage(
-            //            UIImage.GetSystemImage("photo"),
-            //            "Success with image",
-            //            MaskType.Black,
-            //            4000f),
-            //        stack
-            //    );
-            //}
-
-            //MakeButton("Show modal", () =>
-            //    NavigationController.PresentViewController(
-            //        new UINavigationController(new ModalView())
-            //        {
-            //            ModalPresentationStyle = UIModalPresentationStyle.PageSheet
-            //        }, true, null),
-            //    stack
-            //);
         }
 
 
